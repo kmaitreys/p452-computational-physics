1c791dd188d47d5d10fa12977359277d4a7258e7
kmaitreys <peakcipher1729@gmail.com>
2024-02-08 03:33:46 +0530

    added utility to get native range functionality with floating point step size

--- lib/datamodels.py
+++ lib/datamodels.py
@@ -2,6 +2,30 @@ import array
 import random
 
 
+def frange(start, stop=None, step=None):
+    """
+    Helper function to generate a range
+    with step size as a float.
+    """
+    # if set start=0.0 and step = 1.0 if not specified
+    start = float(start)
+    if stop is None:
+        stop = start + 0.0
+        start = 0.0
+    if step is None:
+        step = 1.0
+
+    num_of_steps = 0
+    while True:
+        next_step = float(start + num_of_steps * step)
+        if step > 0 and next_step >= stop:
+            break
+        elif step < 0 and next_step <= stop:
+            break
+        yield next_step
+        num_of_steps += 1
+
+
 class Array:
     @staticmethod
     def zeros(typecode, size):
