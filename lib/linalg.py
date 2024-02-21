8c84b2f197c90ebc95fbb918dc3259f2e4ee88ee
kmaitreys <peakcipher1729@gmail.com>
2024-02-21 13:12:12 +0530

    added `GaussJordan`

--- lib/linalg.py
+++ lib/linalg.py
@@ -1,5 +1,53 @@
-def gauss_jordan():
-    pass
+from datamodels import Array, Matrix
+
+
+class GaussJordan:
+    def __init__(self, matrix: Matrix, vector: Array):
+        self.matrix = matrix
+        self.vector = vector
+        self.augmat = Matrix(matrix.nrows, matrix.ncols + 1)
+        for i in range(matrix.nrows):
+            for j in range(matrix.ncols):
+                self.augmat.data[i][j] = matrix.data[i][j]
+            self.augmat.data[i][matrix.ncols] = vector[i]
+
+    def solve(self):
+        i = 0
+        n = self.matrix.nrows
+        m = n - 1
+        solution = Array.zeros("d", n)
+        # Gaussian elimination
+        while i < n:
+            # Partial pivoting
+            for p in range(i + 1, n):
+                if abs(self.augmat[i, i] < abs(self.augmat[p, i])):
+                    # Swap rows
+                    self.augmat[i], self.augmat[p] = self.augmat[p], self.augmat[i]
+
+            if self.augmat[i, i] == 0.0:
+                raise ZeroDivisionError(
+                    f"{self.augmat} has a zero diagonal element at {i}, {i}"
+                )
+
+            for j in range(i + 1, n):
+                scaling_factor = self.augmat[j, i] / self.augmat[i, i]
+                self.augmat[j] = self.augmat[j] - (scaling_factor * self.augmat[i])
+
+            i += 1
+
+        # Back substitution
+        solution[m] = self.augmat[m, n] / self.augmat[m, m]
+
+        for k in range(n-2, -1, -1):
+            solution[k] = self.augmat[k, n]
+            for j in range(k+1, n):
+                solution[k] -= self.augmat[k, j] * solution[j]
+            
+            solution[k] /= self.augmat[k, k]
+
+        return solution
+
+
 
 def lu_decomposition():
     pass
