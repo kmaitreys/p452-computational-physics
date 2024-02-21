9531080d0d68b65e0d4c2d8bf623d2546b85201e
kmaitreys <peakcipher1729@gmail.com>
2024-02-21 16:38:31 +0530

    added `LUSolve`

--- lib/linalg.py
+++ lib/linalg.py
@@ -4,9 +4,9 @@ from datamodels import Array, Matrix
 class GaussJordan:
     """
     Solve a system of linear equations using the Gauss-Jordan Elimination method.
-    The system is represented as `Ax = b`, where `A` is a matrix of coefficients, 
+    The system is represented as `Ax = b`, where `A` is a matrix of coefficients,
     `x` is the vector of unknowns, which we want to solve for, and `b` is the vector
-    of constants. 
+    of constants.
 
     Attributes
     ----------
@@ -20,9 +20,10 @@ class GaussJordan:
     -------
     solve()
         Solve the system of linear equations using the Gauss-Jordan Elimination method.
-    
-        
+
+
     """
+
     def __init__(self, matrix: Matrix, vector: Array):
         self.matrix = matrix
         self.vector = vector
@@ -59,30 +60,102 @@ class GaussJordan:
         # Back substitution
         solution[m] = self.augmat[m, n] / self.augmat[m, m]
 
-        for k in range(n-2, -1, -1):
+        for k in range(n - 2, -1, -1):
             solution[k] = self.augmat[k, n]
-            for j in range(k+1, n):
+            for j in range(k + 1, n):
                 solution[k] -= self.augmat[k, j] * solution[j]
-            
+
             solution[k] /= self.augmat[k, k]
 
         return solution
 
 
+class LUSolve:
+    """
+    Solve a system of linear equations using the LU Decomposition method.
+    Both `crout` and `doolittle` methods are implemented.
+    """
 
-def lu_decomposition():
+    def __init__(self, matrix: Matrix, vector: Array, method: str):
+        self.matrix = matrix
+        self.vector = vector
+        self.method = method
+        self.pivmat = None
+
+    def solve(self):
+        lower, upper = self.decompose()
+        
+        # Forward substitution
+        n = self.matrix.nrows
+        y = Array.zeros("d", n)
+        y[0] = self.vector[0] / lower[0, 0]
+        for i in range(1, n):
+            sum_1 = sum(lower[i, j] * y[j] for j in range(i))
+            y[i] = (self.vector[i] - sum_1) / lower[i, i]
+        
+        # Back substitution
+        x = Array.zeros("d", n)
+        x[n-1] = y[n-1] / upper[n-1, n-1]
+        for i in range(n-2, -1, -1):
+            sum_2 = sum(upper[i, j] * x[j] for j in range(i+1, n))
+            x[i] = (y[i] - sum_2) / upper[i, i]
+
+        return x
+    
+    def decompose(self):
+        lower = Matrix(self.matrix.nrows, self.matrix.ncols)
+        upper = Matrix(self.matrix.nrows, self.matrix.ncols)
+        if self.method == "crout":
+            for k in range(self.matrix.nrows):
+                upper[k, k] = 1.0
+                for j in range(k, self.matrix.nrows):
+                    sum_1 = sum(lower[k, s] * upper[s, j] for s in range(1, k-1))
+                    lower[j, k] = self.matrix[k, k] - sum_1
+                
+                for j in range(self.matrix.nrows):
+                    sum_2 = sum(lower[k, s] * upper[s, j] for s in range(1, k-1))
+                    upper[k, j] = (self.matrix[k, j] - sum_2) / lower[k, k]
+
+            return lower, upper
+        elif self.method == "doolittle":
+            for k in range(self.matrix.nrows):
+                lower[k, k] = 1.0
+                for j in range(k, self.matrix.nrows):
+                    sum_1 = sum(lower[k, s] * upper[s, j] for s in range(1, k-1))
+                    upper[k, j] = self.matrix[k, j] - sum_1
+                
+                for j in range(self.matrix.nrows):
+                    sum_2 = sum(lower[k, s] * upper[s, j] for s in range(1, k-1))
+                    lower[j, k] = (self.matrix[j, k] - sum_2) / upper[k, k]
+            
+            return lower, upper
+
+
+lu = LUSolve(
+    matrix=Matrix.from_list(
+        [
+            [0, 2, 5],
+            [3, -1, 2],
+            [1, -1, 3]
+        ]
+    ),
+    vector=Array("d", [1, -2, 3]),
+    method="doolittle"
+)
+
+sol = lu.solve()
+print(sol)
+
+
+class Cholesky:
     pass
 
 
-def cholesky_decomposition():
+class GaussJacobi:
     pass
 
 
-def gauss_jacobi():
-    pass
-
-
-def gauss_seidel():
+class GaussSeidel:
     pass
 
 
