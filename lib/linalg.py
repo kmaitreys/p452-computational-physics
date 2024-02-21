1dc1891489f5b22bc58784a7ac254755dc2b938f
kmaitreys <peakcipher1729@gmail.com>
2024-02-21 13:12:20 +0530

    formatting

--- lib/linalg.py
+++ lib/linalg.py
@@ -52,36 +52,46 @@ class GaussJordan:
 def lu_decomposition():
     pass
 
+
 def cholesky_decomposition():
     pass
+
 
 def gauss_jacobi():
     pass
 
+
 def gauss_seidel():
     pass
+
 
 def conjugate_gradient():
     pass
 
+
 def steepest_descent():
     pass
+
 
 def gmres():
     pass
 
+
 def qr_decomposition():
     pass
+
 
 def gram_schmidt():
     pass
 
+
 def householder():
     pass
+
 
 def crout():
     pass
 
+
 def doolittle():
     pass
-
